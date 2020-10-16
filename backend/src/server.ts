20f22abdfebed48f2e15763dc821e9ebabe44354
Gustavo Silva <gstvds@icloud.com>
2020-10-15 22:51:58 -0300

    feat: Adds OrphanagesController and changes router to routes file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,13 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
 
-const app = express();
+import routes from './routes';
 
+const app = express();
 app.use(express.json());
 
+app.use(routes);
+
 const PORT = process.env.PORT || 3333;
-
-app.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-});
 
 app.listen(PORT);
