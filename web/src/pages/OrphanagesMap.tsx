23657291ff079aa8b119141fa049545ca0954a71
Gustavo Silva <gstvds@icloud.com>
2020-10-16 23:33:16 -0300

    feat: Adds Sidebar component and mapIcon util

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
+
+import mapIcon from '../utils/mapIcon';
 
 import mapMarkerImg from '../images/map-marker.svg';
-
-import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
-
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
-});
 
 const OrphanagesMap = () => {
   return (
