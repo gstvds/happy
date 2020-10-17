daacddaba8f26a53d38eb9d91c8536dc79be438b
Gustavo Silva <gstvds@icloud.com>
2020-10-17 14:17:33 -0300

    fix: Fixes open_on_weekends

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,11 +11,11 @@ import errorHandler from './errors/handles';
 
 const app = express();
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-app.use(cors());
 
 const PORT = process.env.PORT || 3333;
 
