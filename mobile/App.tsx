4ecb9b75de9143cc2f4456dcb65001a7952d6fc4
Gustavo Silva <gstvds@icloud.com>
2020-10-18 16:02:33 -0300

    feat: Adds map with marker and footer

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import {
+  useFonts,
+  Nunito_700Bold,
+  Nunito_600SemiBold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
 
-export default function App() {
+import mapMarker from './src/images/map-marker.png';
+import fonts from './src/helpers/fonts';
+
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Nunito_700Bold,
+    Nunito_600SemiBold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator color="#000" />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.5542445,
+          longitude: -47.446738,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: -22.5542445,
+            longitude: -47.446738,
+          }}
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.2,
+            y: 0.7,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar Doce Lar</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+        <TouchableOpacity
+          style={styles.createOrphanageButton}
+          onPress={() => {}}
+        >
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
     justifyContent: 'center',
   },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+    fontFamily: fonts.nunito_bold,
+  },
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+  },
+  footerText: {
+    color: '#8fa7b3',
+    fontFamily: fonts.nunito_bold,
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
+
+export default App;
---
+++ mobile/src/helpers/fonts.ts
@@ -0,0 +1,5 @@
+export default {
+  nunito_semibold: 'Nunito_600SemiBold',
+  nunito_bold: 'Nunito_700Bold',
+  nunito_extrabold: 'Nunito_800ExtraBold',
+};
